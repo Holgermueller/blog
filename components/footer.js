6d3d570923083ce7ddfc4817254e4c875afd5fd5
holgermueller <9jholger@gmail.com>
2023-12-03 16:49:06 -0600

    added social links and did some styling

--- components/footer.js
+++ components/footer.js
@@ -1,3 +1,53 @@
+import { FaInstagram } from "react-icons/fa";
+import { FaTumblr } from "react-icons/fa";
+import { FaDeviantart } from "react-icons/fa";
+import { FaBehance } from "react-icons/fa";
+import { FaCoffee } from "react-icons/fa";
+import { FaEtsy } from "react-icons/fa";
+import { FaTshirt } from "react-icons/fa";
+import Link from "next/link";
+
 export default function Footer() {
-  return <footer>Built with NextJS | &copy; 2023 Holger Mueller</footer>;
+  return (
+    <footer>
+      <div className="footer-socials">
+        <Link href="https://www.instagram.com/holgermmueller/" target="_blank">
+          <FaInstagram />
+        </Link>
+
+        <Link href="https://www.behance.net/holgermueller1" target="_blank">
+          <FaBehance />
+        </Link>
+
+        <Link href="https://ko-fi.com/holgermuellerart" target="_blank">
+          <FaCoffee />
+        </Link>
+
+        <Link
+          href="https://www.etsy.com/shop/HolgerMuellerArt?ref=simple-shop-header-name&listing_id=1449453579"
+          target="_blank"
+        >
+          <FaEtsy />
+        </Link>
+
+        <Link href="https://www.teepublic.com/user/hmshirts" target="_blank">
+          <FaTshirt />
+        </Link>
+
+        <Link
+          href="https://www.tumblr.com/blog/holgermuellerartist"
+          target="_blank"
+        >
+          <FaTumblr />
+        </Link>
+
+        <Link href="https://www.deviantart.com/holgermueller" target="_blank">
+          <FaDeviantart />
+        </Link>
+      </div>
+      <div className="copy">
+        Built with NextJS | Copyright &copy; 2023 Holger Mueller
+      </div>
+    </footer>
+  );
 }
--- pages/contact/index.js
+++ pages/contact/index.js
@@ -1,6 +1,15 @@
 import Header from "@/components/header";
 import Nav from "@/components/nav";
 import Footer from "@/components/footer";
+import Link from "next/link";
+import { FaInstagram } from "react-icons/fa";
+import { FaTumblr } from "react-icons/fa";
+import { FaDeviantart } from "react-icons/fa";
+import { FaBehance } from "react-icons/fa";
+import { FaCoffee } from "react-icons/fa";
+import { FaEtsy } from "react-icons/fa";
+import { FaTshirt } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
 
 export default function Contact() {
   return (
@@ -9,7 +18,56 @@ export default function Contact() {
       <Nav />
       <h2 className="section-header">Contact</h2>
       <section>
-        <h3 className="sub-header">The best ways to get in touch with me</h3>
+        <h3 className="sub-header">
+          You can connect with me on these socials:
+        </h3>
+        <div className="link-container">
+          <Link
+            href="https://www.instagram.com/holgermmueller/"
+            target="_blank"
+          >
+            <FaInstagram />
+          </Link>
+
+          <Link href="https://www.behance.net/holgermueller1" target="_blank">
+            <FaBehance />
+          </Link>
+
+          <Link href="https://ko-fi.com/holgermuellerart" target="_blank">
+            <FaCoffee />
+          </Link>
+
+          <Link
+            href="https://www.etsy.com/shop/HolgerMuellerArt?ref=simple-shop-header-name&listing_id=1449453579"
+            target="_blank"
+          >
+            <FaEtsy />
+          </Link>
+
+          <Link href="https://www.teepublic.com/user/hmshirts" target="_blank">
+            <FaTshirt />
+          </Link>
+
+          <Link
+            href="https://www.tumblr.com/blog/holgermuellerartist"
+            target="_blank"
+          >
+            <FaTumblr />
+          </Link>
+
+          <Link href="https://www.deviantart.com/holgermueller" target="_blank">
+            <FaDeviantart />
+          </Link>
+        </div>
+
+        <div className="mail-section">
+          <h3 className="sub-header">Or you can shoot me a quick email:</h3>
+          <div className="mail-container">
+            <Link target="_blank" href="mailto:9jholger@gmail.com">
+              <MdEmail />
+            </Link>
+          </div>
+        </div>
       </section>
 
       <Footer />
